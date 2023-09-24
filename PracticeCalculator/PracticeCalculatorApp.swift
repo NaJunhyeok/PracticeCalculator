61cae8158ccedaadabbb6a67151c43fc3ca208d3
NaJunhyeok <njs4922@gmail.com>
2023-09-24 20:47:53 +0900

    Initial Commit

---
+++ PracticeCalculator/PracticeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeCalculatorApp.swift
+//  PracticeCalculator
+//
+//  Created by 나준혁 on 2023/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
